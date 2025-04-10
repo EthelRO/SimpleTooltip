3f19fbe29a3f3039c60a2b40711e8859c2fbea7f
Igor Ayres <ayresigo@gmail.com>
2025-04-10 15:34:44 -0300

    refactor: remove item-tooltip entries from localization files
    - Removed 'item-tooltip' entries from English and Brazilian Portuguese localization in ItemDescription.i18n.magic.php to streamline the localization process following the removal of the deprecated parser function.

--- ItemDescription.i18n.magic.php
+++ ItemDescription.i18n.magic.php
@@ -10,14 +10,12 @@ $magicWords = [];
 
 /** English (English) */
 $magicWords['en'] = [
-	'item-tooltip' => [ 0, 'item-tooltip' ],
 	'tip-item' => [ 0, 'tip-item' ],
 	'item' => [ 0, 'item' ],
 ];
 
 /** Português do Brasil (Brazilian Portuguese) */
 $magicWords['pt-br'] = [
-	'item-tooltip' => [ 0, 'item-tooltip' ],
 	'tip-item' => [ 0, 'tip-item' ],
 	'item' => [ 0, 'item' ],
 ]; 
